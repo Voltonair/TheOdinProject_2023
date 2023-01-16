669b3fd29e027b8b4d1f0e949a8e59ad8c3b04ee
Voltonair <martinarmask@gmail.com>
2023-01-16 15:26:42 +0000

    Finish rock paper scissors exercise

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -1,78 +1,87 @@
+console.log("Hi");
+
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let playerName = "";
 
 function getComputerChoice() {
-    let random = choices[Math.floor(Math.random() * choices.length)];
-    console.log(random);
-    return random;
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
 }
 
+function getPlayerChoice() {
+    let validateInput = false;
+    
+    while (validateInput == false) {
+        const playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
+        if (playerChoice == null || !choices.includes(playerChoice)) {
+            alert("Wrong choice!");
+            continue;
+        }
+
+        let playerChoiceLower = playerChoice.toLowerCase();
+
+        if (choices.includes(playerChoiceLower)) {
+            validateInput = true;
+            return playerChoiceLower;
+        }
+    }
+}
+
+function getPlayerName() {
+    const playerName = prompt("What is your name?");
+    console.log(playerName);
+    return playerName;
+}
+
+function getScore() {
+    if (playerScore > computerScore) {
+        return "Player wins!!!";
+    } else if (playerScore < computerScore) {
+        return "You lose!!!";
+    } else {
+        return "Draw!!!";
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
-    
-    
     if (playerSelection === computerSelection) {
         return "It's a tie";
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
             playerScore++;
-            return `${playerName} wins! ${playerSelection} beats ${computerSelection}!`;
+            return "You win the round!";
         } else if (computerSelection === "paper") {
             computerScore++;
-            return `${playerName} lose! ${computerSelection} beats ${playerSelection}!`;
+            return "You lose the round!";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            return `${playerName} wins! ${playerSelection} beats ${computerSelection}!`;
+            return "You win the round!";
         } else if (computerSelection === "scissors") {
             computerScore++;
-            return `${playerName} lose! ${computerSelection} beats ${playerSelection}!`;
+            return "You lose the round!";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
             playerScore++;
-            return `${playerName} wins! ${playerSelection} beats ${computerSelection}!`;
+            return "You win the round!";
         } else if (computerSelection === "rock") {
             computerScore++;
-            return `${playerName} lose! ${computerSelection} beats ${playerSelection}!`;
+            return "You lose the round!";
         }
     }
-    
-}
-
-function playerChoice() {
-    const userChoice = prompt("Rock, paper, scissors").toLowerCase();
-
-    if (choices.includes(userChoice)) {
-        return userChoice;
-    }
-}
-
-function getPlayerName() {
-    playerName = prompt("What is your name?");
-    return playerName;
 }
 
 function game () {
     getPlayerName();
-    console.log(`Welcome ${playerName}`)
-
-    for (i = 0; i < 3; i++) {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-        const playerSelection = playerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }
-    
-    if (playerScore > computerScore) {
-        return "Game over! Player wins!";
-    } else if (playerScore < computerScore) {
-        return "Game over! Player loses!";
-    } else {
-        return "Game over, draw!";
-    }
+    console.log(getScore());
 }
 
-console.log(game());
+game();
